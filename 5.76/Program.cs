a2d157c0d89575e2180d63e6210357c6c4a5cc7b
danil <danil@DESKTOP-KLFI3LI>
2022-11-13 23:29:44 +0200

    Добавьте файлы проекта.

---
+++ 5.76/Program.cs
@@ -0,0 +1,11 @@
+﻿//Голубихин Данила 22-ИСП-2/2 Задание 5.76
+Console.Write("Введите а: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+double b = 1;
+for(int i = 1; i <= n; i++)
+{
+    b *= a;
+    Console.WriteLine(b);
+}
